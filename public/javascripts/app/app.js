65701381744bb6d042e1bb93886c59b63f6f4930
Pete Dunlap <petedunlap@gmail.com>
2013-12-09 23:10:57 -0600

    yummly api search request returning json

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -2,21 +2,30 @@
 
 $(document).ready(initialize);
 
-var socket;
+var app_id = "8f303a51";
+var app_key = "aff06d00c9a604466f6f1cd0e42927c7";
+var apiBase = "http://api.yummly.com/v1/api/recipes?";
+var authentication = "_app_id=" + app_id + "&_app_key=" + app_key;
 
 function initialize(){
   $(document).foundation();
-  initializeSocketIO();
+  $('#searchButton').on('click',clickSearch);
 }
 
-function initializeSocketIO(){
-  var port = window.location.port ? window.location.port : '80';
-  var url = window.location.protocol + '//' + window.location.hostname + ':' + port + '/app';
+function clickSearch(){
+  var searchTerm = getValue('#searchTerm');
+  searchTerm = searchTerm.replace(/ /g, '+');
+  url = apiBase + authentication + "&q=" + searchTerm;
+  console.log(url);
+  $.ajax({type: "GET", url: url, dataType: "jsonp", success: function (data) {
+      console.log(data);
+      for(var i = 0 ; i < data.matches.length; i++){
+        var $li = $('<li>' + data.matches[i].recipeName + '</li>');
+        $('#recipes').append($li);
+      }
+    }
+  });
 
-  socket = io.connect(url);
-  socket.on('connected', socketConnected);
 }
 
-function socketConnected(data){
-  console.log(data);
-}
+///////////////////////////////////////////////////////////////////////////
