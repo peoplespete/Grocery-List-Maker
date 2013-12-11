1fa07612ef8b01ac059ec80b24412bbd15b4d8cb
Pete Dunlap <petedunlap@gmail.com>
2013-12-11 13:03:37 -0600

    working on UX

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -1,3 +1,4 @@
+
 /* global document, window, io */
 
 $(document).ready(initialize);
@@ -25,11 +26,16 @@ function clickSearch(){
   var searchTerm = getValue('#searchTerm');
   searchTerm = searchTerm.replace(/ /g, '+');
   var url = apiBase + "/recipes?" + authentication + "&q=" + searchTerm;
-  // console.log(url);
   $.ajax({type: "GET", url: url, dataType: "jsonp", success: function (data) {
-      // console.log(data);
       for(var i = 0 ; i < data.matches.length; i++){
-        var $li = $('<li data_id = ' + data.matches[i].id + '>' + data.matches[i].recipeName + '</li>');
+        var sz = "90";
+        var pic;
+        if(data.matches[i].imageUrlsBySize){
+          pic = data.matches[i].imageUrlsBySize[sz];
+        }else{
+          pic = '/images/item-placeholder.png';
+        }
+        var $li = $('<li data_id = ' + data.matches[i].id + '>' + '<img src=' + pic + '>' + data.matches[i].recipeName + '</li>');
         $('#searchMatches').append($li);
       }
       matches = data.matches;
@@ -39,8 +45,10 @@ function clickSearch(){
 }
 
 function clickRecipe(){
-  var id = $(this).attr('data_id');
+  $('#list').show().removeClass('hidden');
+  $('#choose').show().removeClass('hidden');
  // push ingredients to shopping list from matches array local
+  var id = $(this).attr('data_id');
   var recipeResponse = _.find(matches, function(item){
     return item.id == id;
   });
@@ -49,19 +57,17 @@ function clickRecipe(){
   clickedRecipe.name = recipeResponse.recipeName;
 
   var url = apiBase + "/recipe/" + id + "?" + authentication;
-  // console.log(url);
   $.ajax({type: "GET", url: url, dataType: "jsonp", success: function (data) {
-      // console.log(data);
       clickedRecipe.id = data.id;
       clickedRecipe.link = data.source.sourceRecipeUrl;
       clickedRecipe.measuredIngredients = data.ingredientLines;
-      // console.log(clickedRecipe);
       recipes.push(clickedRecipe);
       var $clickedRecipe = $('<li>');
       $clickedRecipe.attr('data-id',clickedRecipe.id);
-      $clickedRecipe.append('<a href=' + clickedRecipe.link + ' target="_blank">' + clickedRecipe.name + '</a>');
+      var sz = "90";
+      $clickedRecipe.append('<img src=' + recipeResponse.imageUrlsBySize[sz] + '>' + '<a href=' + clickedRecipe.link + ' target="_blank">' + clickedRecipe.name + '</a>');
  // add delete button for each item you put in
-      var $delete = $('<input type="button" value="Remove" class="button small radius alert" >');
+      var $delete = $('<input type="button" value="x" class="button tiny radius alert" >');
       $clickedRecipe.append($delete);
       $('#chosenRecipes').append($clickedRecipe);
       // $('#chosenAd').append(data.attribution.html);
@@ -84,6 +90,9 @@ function clickDeleteChosenRecipe(){
   clickPrint();
   if(recipes.length === 0){
     $('#openLinksButton').hide().addClass('hidden');
+    $('#list').hide().addClass('hidden');
+    $('#choose').hide().addClass('hidden');
+    $('#searchTerm').focus();
   }
 }
 
@@ -99,12 +108,12 @@ function clickPrint(){
         // else add it to list with ingred and amount
     _.forEach(r.ingredients, function(ingred){
       var found = false;
+      var re = new RegExp(' ' + ingred, 'g');
+      var measuredIngred;
       _.forEach(list, function(item){
         if(ingred == item.ingredient){
-          var re = new RegExp(ingred, 'g');
-          var measuredIngred;
           _.forEach(r.measuredIngredients, function(mIngred){
-            if(mIngred.indexOf(ingred) != -1){
+            if(mIngred.indexOf(' ' + ingred) != -1){
               measuredIngred = mIngred.replace(re,'')
               item.amounts.push(measuredIngred);
               found = true;
@@ -116,10 +125,8 @@ function clickPrint(){
         item = {};
         item.ingredient = ingred;
         item.amounts = [];
-        var re = new RegExp(ingred, 'g');
-        var measuredIngred;
         _.forEach(r.measuredIngredients, function(mIngred){
-          if(mIngred.indexOf(ingred) != -1){
+          if(mIngred.indexOf(' ' + ingred) != -1){
             measuredIngred = mIngred.replace(re,'');
             item.amounts.push(measuredIngred);
           }
@@ -132,24 +139,23 @@ function clickPrint(){
       var $li = $('<li>');
       var $ingredient = $('<div>');
       $ingredient.text(item.ingredient);
-      $ingredient.text(item.ingredient);
+      $ingredient.addClass('match');
       var $amounts = $('<div>');
       _.forEach(item.amounts, function(a){
         var $amount = $('<div>');
+        $amount.addClass('amount');
         $amount.text(a);
         $amounts.append($amount);
       });
 
-      var $delete = $('<input type="button" value="Remove" class="button small radius alert" >');
+      var $delete = $('<input type="button" value="x" class="button tiny radius alert" >');
       $li.append($ingredient, $amounts, $delete);
       $('#groceryList').append($li);
   });
-  console.log(list);
 }
 
 function clickOpenLinks(){
   _.forEach(recipes, function(r){
-    console.log("Opening link to: " + r.name);
     window.open(r.link,'_blank');
   });
 }
